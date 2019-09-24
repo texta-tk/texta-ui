468396e614b3d61791acb2b6e5f9dfbb9598c473
Gert Paimla <gertpaimla@gmail.com>
2019-09-24 17:01:17 +0300

    neurotagger ui

--- src/app/tagger/tagger/tag-text-dialog/tag-text-dialog.component.spec.ts
+++ src/app/tagger/tagger/tag-text-dialog/tag-text-dialog.component.spec.ts
@@ -3,7 +3,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {TagTextDialogComponent} from './tag-text-dialog.component';
 import {RouterTestingModule} from '@angular/router/testing';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {SharedModule} from '../../../shared/shared.module';
+import {SharedModule} from '../../../shared.module';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 
