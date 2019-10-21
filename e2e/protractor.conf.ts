1ff6be95b279f4aabf1d2bfde9834605506205b8
Gert Paimla <gertpaimla@gmail.com>
2019-10-21 15:24:50 +0300

    protractor conf to ts

--- e2e/protractor.conf.ts
+++ e2e/protractor.conf.ts
@@ -1,5 +1,8 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
+import {browser, by, element} from "protractor";
+import {protractor} from "protractor/built/ptor";
+
 const {SpecReporter} = require('jasmine-spec-reporter');
 
 exports.config = {
@@ -16,7 +19,7 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: () => {
+    print: function () {
     }
   },
   onPrepare: async () => {
@@ -24,5 +27,14 @@ exports.config = {
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
+    await browser.driver.get('http://localhost:4200/'); // todo, default? env file!
+    const login = await browser.driver.findElement(by.id('loginButton'));
+    login.click();
+    await browser.driver.findElement(by.css('input[formcontrolname=usernameFormControl')).sendKeys('test'); // todo
+    await browser.driver.findElement(by.css('input[formcontrolname=passwordFormControl')).sendKeys('test');
+    await browser.driver.findElement(by.tagName('form')).submit();
+    const until = protractor.ExpectedConditions;
+    const logoutButton = element(by.className('logout'));
+    await browser.driver.wait(until.presenceOf(logoutButton), 5000, 'wait for logout div to be present timed out');
   }
 };
