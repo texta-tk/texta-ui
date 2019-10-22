81d8fb75b06338fbd82be969c5587e626e89c59c
ranetp <ranetpuusepp@gmail.com>
2019-10-22 18:21:49 +0300

    Test fixes

--- src/app/tagger/tagger-group/tagger-group-tag-doc-dialog/tagger-group-tag-doc-dialog.component.spec.ts
+++ src/app/tagger/tagger-group/tagger-group-tag-doc-dialog/tagger-group-tag-doc-dialog.component.spec.ts
@@ -1,14 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaggerGroupTagDocDialogComponent } from './tagger-group-tag-doc-dialog.component';
+import { TaggerGroup } from 'src/app/shared/types/tasks/Tagger';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 describe('TaggerGroupTagDocDialogComponent', () => {
   let component: TaggerGroupTagDocDialogComponent;
   let fixture: ComponentFixture<TaggerGroupTagDocDialogComponent>;
+  const mockDialogRef = {
+    close: jasmine.createSpy('close')
+  };
+
+  const data = {currentProjectId: 1, tagger: new TaggerGroup()};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TaggerGroupTagDocDialogComponent ]
+      declarations: [ TaggerGroupTagDocDialogComponent ],
+      imports: [
+        SharedModule, HttpClientTestingModule, RouterTestingModule, BrowserAnimationsModule
+      ], providers: [
+        {
+          provide: MatDialogRef,
+          useValue: mockDialogRef
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: data
+        }]
     })
     .compileComponents();
   }));
--- src/app/tagger/tagger-group/tagger-group-tag-random-doc-dialog/tagger-group-tag-random-doc-dialog.component.spec.ts
+++ src/app/tagger/tagger-group/tagger-group-tag-random-doc-dialog/tagger-group-tag-random-doc-dialog.component.spec.ts
@@ -1,14 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TaggerGroupTagRandomDocDialogComponent } from './tagger-group-tag-random-doc-dialog.component';
+import { TaggerGroup } from 'src/app/shared/types/tasks/Tagger';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 describe('TaggerGroupTagRandomDocDialogComponent', () => {
   let component: TaggerGroupTagRandomDocDialogComponent;
   let fixture: ComponentFixture<TaggerGroupTagRandomDocDialogComponent>;
+  const mockDialogRef = {
+    close: jasmine.createSpy('close')
+  };
+
+  const data = {currentProjectId: 1, tagger: new TaggerGroup()};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TaggerGroupTagRandomDocDialogComponent ]
+      declarations: [ TaggerGroupTagRandomDocDialogComponent ],
+      imports: [
+        SharedModule, HttpClientTestingModule, RouterTestingModule, BrowserAnimationsModule
+      ], providers: [
+        {
+          provide: MatDialogRef,
+          useValue: mockDialogRef
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: data
+        }]
     })
     .compileComponents();
   }));
