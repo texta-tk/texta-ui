4b597677e5353a7cca05bb9cc655616011ce169b
unknown <ranetpuusepp@gmail.com>
2019-11-19 13:08:59 +0200

    Fix tests

--- src/app/torch-tagger/create-torch-tagger-dialog/create-torch-tagger-dialog.component.spec.ts
+++ src/app/torch-tagger/create-torch-tagger-dialog/create-torch-tagger-dialog.component.spec.ts
@@ -1,14 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateTorchTaggerDialogComponent } from './create-torch-tagger-dialog.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogRef } from '@angular/material';
 
 describe('CreateTorchTaggerDialogComponent', () => {
   let component: CreateTorchTaggerDialogComponent;
   let fixture: ComponentFixture<CreateTorchTaggerDialogComponent>;
-
+  const mockDialogRef = {
+    close: jasmine.createSpy('close')
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateTorchTaggerDialogComponent ]
+      imports: [
+        SharedModule, HttpClientTestingModule, RouterTestingModule, BrowserAnimationsModule
+      ],
+      declarations: [ CreateTorchTaggerDialogComponent ],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: mockDialogRef
+        }]
     })
     .compileComponents();
   }));
--- src/app/torch-tagger/torch-tagger.service.spec.ts
+++ src/app/torch-tagger/torch-tagger.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TorchTaggerService } from './torch-tagger.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from '../shared/shared.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('TorchTaggerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({    imports: [
+    RouterTestingModule,
+    SharedModule,
+    HttpClientTestingModule
+  ]}));
 
   it('should be created', () => {
     const service: TorchTaggerService = TestBed.get(TorchTaggerService);
