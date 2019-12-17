500e66eb5e9cba57d12d081386ad6d4bf74bf84a
Tõnu Raitviir <jussuf@dev.texta.ee>
2019-12-17 16:28:00 +0200

    Use # instead if / in sed, so that we don't have to use escapes in URL

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,6 @@
 set -xe
 : "${TEXTA_API_URL?Need an api url}"
 
-sed -i "s/REST_API_URL_REPLACE/$TEXTA_API_URL/g" /usr/share/nginx/html/main*.js
+sed -i "s#REST_API_URL_REPLACE#$TEXTA_API_URL#g" /usr/share/nginx/html/main*.js
 
 exec "$@"
