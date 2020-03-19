6bdff2be51fcac16cef64c4f7bfa2fc4243c031a
sfhsdfasg <gertpaimla@gmail.com>
2020-03-19 13:57:27 +0200

    health fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,48 +1,48 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
-import { ProjectService } from '../core/projects/project.service';
-import { Health } from '../shared/types/Project';
-import { HttpErrorResponse } from '@angular/common/http';
-import { UserStore } from '../core/users/user.store';
-import { UserProfile } from '../shared/types/UserProfile';
-import { Subscription } from 'rxjs';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ProjectService} from '../core/projects/project.service';
+import {Health} from '../shared/types/Project';
+import {HttpErrorResponse} from '@angular/common/http';
+import {UserStore} from '../core/users/user.store';
+import {UserProfile} from '../shared/types/UserProfile';
+import {Subscription} from 'rxjs';
 import * as projectPackage from '../../../package.json';
 
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  health: Health;
-  unreachable: boolean;
-  userSub: Subscription;
-  user: UserProfile;
-  frontVersion = projectPackage.version;
+    health: Health;
+    unreachable: boolean;
+    userSub: Subscription;
+    user: UserProfile;
+    frontVersion = projectPackage.version;
 
-  constructor(private projectService: ProjectService, private userStore: UserStore) {
-    
-  }
+    constructor(private projectService: ProjectService, private userStore: UserStore) {
 
-  ngOnInit() {
-    this.userSub = this.userStore.getCurrentUser().subscribe((user: UserProfile) => {
-      this.user = user;
-      this.projectService.getHealth().subscribe((resp: Health | HttpErrorResponse) => {
-        if (resp && !(resp instanceof HttpErrorResponse)) {
-          this.health = resp;
-          this.unreachable = false;
-        } else {
-          this.unreachable = true;
-        }
-      });
-    });
-  }
+    }
 
-  ngOnDestroy(): void {
-    this.userSub.unsubscribe();
-  }
+    ngOnInit() {
+        this.userSub = this.userStore.getCurrentUser().subscribe((user: UserProfile) => {
+            this.user = user;
+            this.projectService.getHealth().subscribe((resp: Health | HttpErrorResponse) => {
+                if (resp && !(resp instanceof HttpErrorResponse)) {
+                    this.health = resp;
+                    this.unreachable = false;
+                } else {
+                    this.unreachable = true;
+                }
+            });
+        });
+    }
 
-  round(value: number) {
-    return Math.round(value);
-  }
+    ngOnDestroy(): void {
+        this.userSub.unsubscribe();
+    }
+
+    round(value: number) {
+        return Math.round(value);
+    }
 }
--- src/app/shared/types/Project.ts
+++ src/app/shared/types/Project.ts
@@ -82,35 +82,41 @@ export class ProjectField {
 
 // for the project/{id}/heath endpoint
 export interface Health {
-  elastic: {
-    url: string;
-    alive: boolean;
-    status: {
-      name: string;
-      cluster_name: string;
-      cluster_uuid: string;
-      version: {
-        number: string;
-        build_flavor: string;
-        build_type: string;
-        build_hash: string;
-        build_date: string;
-        build_snapshot: boolean;
-        lucene_version: string;
-        minimum_wire_compatibility_version: string;
-        minimum_index_compatibility_version: string;
-      };
-      tagline: string;
-    };
+  host: {
+    cpu: { percent: number };
+    gpu: { count: number; devices: string[] };
+    memory: { free: number, total: number, used: number, unit: string };
+    disk: { free: number, total: number, used: number, unit: string };
   };
-  redis: { alive: boolean, version: number, expired_keys: number, used_memory: string, total_memory: string };
-  mlp: { url: string, alive: boolean };
-  version: string;
-  disk: { free: number, total: number, used: number, unit: string };
-  memory: { free: number, total: number, used: number, unit: string };
-  cpu: { percent: number };
-  gpu: { count: number; devices: string[] };
-  active_tasks: number;
+  toolkit: {
+    active_tasks: number;
+    version: string;
+  };
+  services: {
+    elastic: {
+      url: string;
+      alive: boolean;
+      status: {
+        name: string;
+        cluster_name: string;
+        cluster_uuid: string;
+        version: {
+          number: string;
+          build_flavor: string;
+          build_type: string;
+          build_hash: string;
+          build_date: string;
+          build_snapshot: boolean;
+          lucene_version: string;
+          minimum_wire_compatibility_version: string;
+          minimum_index_compatibility_version: string;
+        };
+        tagline: string;
+      };
+    };
+    mlp: { url: string, alive: boolean, status: { loaded_entities: string[], version: string, service: string } };
+    redis: { alive: boolean, version: number, expired_keys: number, used_memory: string, total_memory: string };
+  };
 }
 
 // this interface is for localstorage
