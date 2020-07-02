4679a082e93b3e856499246323b0620cc7e75730
sfhsdfasg <gertpaimla@gmail.com>
2020-07-02 16:09:34 +0300

    fix karma

--- src/app/searcher/aggregation-results/aggregation-results-tree/aggregation-results-tree.component.spec.ts
+++ src/app/searcher/aggregation-results/aggregation-results-tree/aggregation-results-tree.component.spec.ts
@@ -6,6 +6,8 @@ import {SharedModule} from '../../../shared/shared.module';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {DatePipe} from '@angular/common';
+import {SearcherComponentService} from '../../services/searcher-component.service';
+import {SearchServiceSpy} from '../../services/searcher-component.service.spec';
 
 describe('AggregationResultsTreeComponent', () => {
   let component: AggregationResultsTreeComponent;
@@ -17,6 +19,12 @@ describe('AggregationResultsTreeComponent', () => {
       imports: [
         SharedModule, HttpClientTestingModule, RouterTestingModule, BrowserAnimationsModule
       ], providers: [DatePipe]
+    }).overrideComponent(AggregationResultsTreeComponent, {
+      set: {
+        providers: [
+          {provide: SearcherComponentService, useClass: SearchServiceSpy}
+        ]
+      }
     })
       .compileComponents();
   }));
