45a13a793c345d669165eb8062f46a9024464990
rsirel <rsirel@texta.ee>
2020-09-15 10:34:25 +0300

    update cypress conf

--- src/environments/environment-cypress.prod.ts
+++ src/environments/environment-cypress.prod.ts
@@ -7,10 +7,10 @@ export const environment = {
   // For CF UAA authentication
   useCloudFoundryUAA: true,
   uaaConf: {
-    uaaURL: 'http://localhost:8080/uaa/oauth/authorize',
+    uaaURL: 'http://texta-uaa:8080/uaa/oauth/authorize',
 
     // Callback URL defined on the UAA server, to which the user will be redirected after logging in on UAA
-    redirect_uri: 'http://localhost:8000/api/v1/uaa/callback',
+    redirect_uri: 'http://texta-rest/api/v1/uaa/callback',
     // OAuth 2.0 client application (eg texta_toolkit) id and secret.
     client_id: 'login',
     // OAuth 2.0 scope and response_type
