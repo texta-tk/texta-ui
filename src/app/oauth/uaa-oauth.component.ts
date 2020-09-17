069bdf80b4c96100120fb3f47cb9d8d48cbe38d1
Ranet Puusepp <ranetpuusepp@gmail.com>
2020-09-17 17:58:17 +0300

    Set user after authing with UAA

--- src/app/oauth/uaa-oauth.component.ts
+++ src/app/oauth/uaa-oauth.component.ts
@@ -2,11 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LocalStorageService } from '../core/util/local-storage.service';
 import { UserService } from '../core/users/user.service';
-import { UserStore } from '../core/users/user.store';
 import { HttpErrorResponse } from '@angular/common/http';
-import { UserProfile } from '../shared/types/UserProfile';
-import { LogService } from '../core/util/log.service';
 import { switchMap } from 'rxjs/operators';
+import { LogService } from '../core/util/log.service';
+import { UserStore } from '../core/users/user.store';
 
 @Component({
   selector: 'app-uaa-oauth',
@@ -19,30 +18,31 @@ export class UaaOauthComponent implements OnInit {
     private localStorageService: LocalStorageService,
     private userService: UserService,
     private logService: LogService,
+    private userStore: UserStore,
     private router: Router) { }
 
-  ngOnInit(): void {
-    this.route.queryParams.pipe(switchMap(params => {
-      // Get the tokens from queryParams
-      const accessToken = params['access_token'];
-      const refreshToken = params['refresh_token'];
-
-      // Set them to the localstorage to be used in the interceptor
-      if (refreshToken) {
-        this.localStorageService.setOAuthRefreshToken(refreshToken);
-      }
-
-      if (accessToken) {
-        this.localStorageService.setOAuthAccessToken(accessToken);
-      }
-      return this.userService.getUserProfile();
-    })).subscribe(resp => {
-      if (resp instanceof HttpErrorResponse) {
-        this.logService.snackBarError(resp);
-      } else if (resp) {
-        this.router.navigate(['']);
-      }
-    });
-  }
-
+    ngOnInit(): void {
+      this.route.queryParams.pipe(switchMap(params => {
+        // Get the tokens from queryParams
+        const accessToken = params['access_token'];
+        const refreshToken = params['refresh_token'];
+  
+        // Set them to the localstorage to be used in the interceptor
+        if (refreshToken) {
+          this.localStorageService.setOAuthRefreshToken(refreshToken);
+        }
+  
+        if (accessToken) {
+          this.localStorageService.setOAuthAccessToken(accessToken);
+        }
+        return this.userService.getUserProfile();
+      })).subscribe(resp => {
+        if (resp instanceof HttpErrorResponse) {
+          this.logService.snackBarError(resp);
+        } else if (resp) {
+          this.userStore.setCurrentUser(resp);
+          this.router.navigate(['']);
+        }
+      });
+    }
 }
