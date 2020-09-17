4e0b5e1b589e9c1e6e48609f56720112ba81ff0c
rsirel <rsirel@texta.ee>
2020-09-17 13:17:55 +0300

    reform production env variable parsing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   // For CF UAA authentication
   useCloudFoundryUAA: JSON.parse('TEXTA_USE_UAA_REPLACE'.toLowerCase()),
   uaaConf: {
-    uaaURL: 'TEXTA_UAA_AUTH_URL_REPLACE',
+    uaaURL: 'TEXTA_UAA_URL_REPLACE' + '/oauth/authorize',
     // Callback URL defined on the UAA server, to which the user will be redirected after logging in on UAA
     redirect_uri: 'TEXTA_UAA_REDIRECT_URI_REPLACE',
     // OAuth 2.0 client application (eg texta_toolkit) id and secret.
