1190bc687f032c47893e69d551aa6f12bbf9dda6
Gert Paimla <gertpaimla@gmail.com>
2020-09-30 22:54:47 +0300

    Update cypress/integration/tools/mlp.e2e-spec.js

--- cypress/integration/tools/mlp.e2e-spec.js
+++ cypress/integration/tools/mlp.e2e-spec.js
@@ -49,14 +49,14 @@ describe('mlp should work', function () {
       assert.equal(created.response.body.task.status, 'created');
     });
     cy.get('.mat-header-row > .cdk-column-id').should('be.visible').then(bb => {
-      cy.wrap([0, 0, 0, 0, 0, 0]).each(y => { // hack to wait for task to complete
+      cy.wrap([0, 0, 0, 0, 0, 0, 0, 0, 0]).each(y => { // hack to wait for task to complete
         cy.wrap(bb).click();
         return cy.wait('@getMLPTasks').then((x) => {
           if (x?.response?.body?.results[0]?.task?.status === 'completed') {
             assert.equal(x?.response?.body?.results[0]?.task?.status, 'completed');
             return false;
           }
-          return cy.wait(5000);
+          return cy.wait(10000);
         });
       })
     });
