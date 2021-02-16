4453b157cdb30d900d72b59b023e87437043f7be
Gert Paimla <gertpaimla@gmail.com>
2021-02-16 20:27:09 +0200

    lexicon e2e cypress 6.5

--- cypress/integration/lexicons/lexicons.e2e-spec.js
+++ cypress/integration/lexicons/lexicons.e2e-spec.js
@@ -9,52 +9,49 @@ describe('lexicons should work', function () {
     });
   });
   it('should be able to create a new lexcion, save words, get new suggestions', function () {
-  /*  cy.importTestEmbedding(this.projectId).then(x => {
+    cy.importTestEmbedding(this.projectId).then(x => {
 
       cy.intercept('GET', '**user**').as('getUser');
       cy.intercept('GET', '**get_fields**').as('getProjectIndices');
-      cy.intercept('GET', '**!/lexicons/!**').as('getLexicons');
-      cy.intercept('DELETE', '**!/lexicons/!**').as('deleteLexicons');
-      cy.intercept('POST', '**!/lexicons/!**').as('postLexicons');
-      cy.intercept('PATCH', '**!/lexicons/!**').as('patchLexicons');
+      cy.intercept('GET', '**/lexicons/**').as('getLexicons');
+      cy.intercept('DELETE', '**/lexicons/**').as('deleteLexicons');
+      cy.intercept('POST', '**/lexicons/**').as('postLexicons');
+      cy.intercept('PATCH', '**/lexicons/**').as('patchLexicons');
       cy.visit('/lexicons');
 
       cy.wait('@getProjectIndices');
+      cy.wait('@getLexicons')
       cy.get('[data-cy=appNavbarProjectSelect]').click();
       cy.get('mat-option').contains('integration_test_project').click();
-      cy.get('[data-cy=appLexiconNewListItem]:first() input').should('be.visible').click().type('testLex');
-      cy.get('[data-cy=appLexiconNewListItem]:first() button').should('be.visible').click();
+      cy.wait('@getLexicons')
+      cy.get('[data-cy=appLexiconCreateBtn]').click();
+
+      cy.get('[data-cy=appLexiconCreateDialogDesc]').should('be.visible').click().type('testLex');
+      cy.get('[type="submit"]').click();
       cy.wait('@postLexicons').then(created => {
         expect(created.response.statusCode).to.eq(201);
       });
-      cy.get('[data-cy=appLexiconListItem]:first()').should('be.visible').click();
-      cy.get('[data-cy=appLexiconEmbedding]').should('be.visible').click();
-      cy.get('mat-option:first()').should('be.visible').click();
-      cy.closeCurrentCdkOverlay();
-      cy.get('[data-cy=appLexiconWords]').should('be.visible').click().type('pede');
-      cy.intercept('POST', '**!/predict_similar/').as('predictSimilar');
-      cy.get('[data-cy=appLexiconNewSuggestionsBtn]').should('be.visible').click();
-      cy.wait('@predictSimilar').then(resp=>{
-        expect(resp.response.statusCode).to.eq(200);
+      cy.get('.element-row:first()').click() //pede
+      cy.get('[data-cy=appLexiconEmbedding]').click();
+      cy.get('.mat-option:first()').click();
+      cy.get('[data-cy=appLexiconPositiveUsedWords]').type('pede');
+      cy.intercept('POST', '**/embeddings/**').as('postEmbeddings');
+      cy.get('[data-cy=appLexiconNewSuggestionsBtn]').click();
+      cy.wait('@postEmbeddings').then(intercepted=>{
+        cy.wrap(intercepted).its('response.statusCode').should('eq', 200);
+        cy.get('.suggestions > .flex-col > :nth-child(1)').click();
       });
-      cy.get('[data-cy=appLexiconPredictions]:first()').should('be.visible').click();
-      cy.get('[data-cy=appLexiconNewSuggestionsBtn]').should('be.visible').click();
-      cy.wait('@predictSimilar').then(resp=>{
-        expect(resp.response.statusCode).to.eq(200);
+      cy.get('[data-cy=appLexiconSaveBtn]').click();
+      cy.wait('@patchLexicons').then(intercepted=>{
+        cy.wrap(intercepted).its('response.statusCode').should('eq', 200);
       });
-      cy.get('[data-cy=appLexiconNegatives]').should('not.have.length',0);
-      cy.get('[data-cy=appLexiconSaveBtn]').should('be.visible').click();
-      cy.wait('@patchLexicons').then(resp=>{
-        expect(resp.response.statusCode).to.eq(200);
+      cy.get('.breadcrumb > :nth-child(1)').click();
+      cy.wait('@getLexicons');
+      cy.get('[data-cy=appLexiconMenuDelete]:first()').click();
+      cy.get('[type="submit"]').click();
+      cy.wait('@deleteLexicons').then(intercepted=>{
+        cy.wrap(intercepted).its('response.statusCode').should('eq', 204);
       });
-      cy.get('[data-cy=appLexiconNewListItem]:first() input').should('be.visible').click().type('newLex');
-      cy.get('[data-cy=appLexiconNewListItem]:first() button').should('be.visible').click();
-      cy.wait('@postLexicons').then(created => {
-        expect(created.response.statusCode).to.eq(201);
-      });
-      cy.get('[data-cy=appLexiconListItem]').contains('newLex').should('be.visible').click();
-      cy.get('[data-cy=appLexiconListItem]').contains('testLex').should('be.visible').click();
-      cy.get('[data-cy=appLexiconNegatives]').should('not.have.length',0);
-    });*/
+    });
   });
 });
