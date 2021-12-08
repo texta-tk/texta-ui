33b6c7d2514df9158d1357446602dbc562793482
gpaimla <gertpaimla@gmail.com>
2021-12-08 15:51:16 +0200

    test(e2e): add test logger

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,5 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-file-upload';
+require('cypress-terminal-report/src/installLogsCollector')();
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
