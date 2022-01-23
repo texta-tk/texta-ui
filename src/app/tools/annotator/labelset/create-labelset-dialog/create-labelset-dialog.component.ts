49ee1b8133fa1ff0a176a9997e8fc24174e0dbd9
gpaimla <gertpaimla@gmail.com>
2022-01-24 00:15:28 +0200

    feat(Annotator): annotator create labelset: added fact name as input
    version 1.6.28

--- src/app/tools/annotator/labelset/create-labelset-dialog/create-labelset-dialog.component.ts
+++ src/app/tools/annotator/labelset/create-labelset-dialog/create-labelset-dialog.component.ts
@@ -8,15 +8,20 @@ import {LexiconService} from '../../../../core/lexicon/lexicon.service';
 import {MatDialogRef} from '@angular/material/dialog';
 import {ProjectStore} from '../../../../core/projects/project.store';
 import {MatMenuTrigger} from '@angular/material/menu';
-import {switchMap, takeUntil} from 'rxjs/operators';
+import {filter, switchMap, take, takeUntil} from 'rxjs/operators';
 import {forkJoin, of, Subject} from 'rxjs';
 import {HttpErrorResponse} from '@angular/common/http';
-import {Project} from '../../../../shared/types/Project';
+import {Project, ProjectIndex} from '../../../../shared/types/Project';
 import {AnnotatorService} from '../../../../core/tools/annotator/annotator.service';
+import {ProjectService} from '../../../../core/projects/project.service';
+import {UtilityFunctions} from '../../../../shared/UtilityFunctions';
 
 interface OnSubmitParams {
   categoryFormControl: string;
   valuesFormControl: string;
+  valueLimitFormControl: number;
+  indicesFormControl: ProjectIndex[];
+  factNameFormControl: string[];
 }
 
 @Component({
@@ -27,17 +32,23 @@ interface OnSubmitParams {
 export class CreateLabelsetDialogComponent implements OnInit {
   matcher: ErrorStateMatcher = new LiveErrorStateMatcher();
   labelSetForm = new FormGroup({
+    indicesFormControl: new FormControl([]),
     categoryFormControl: new FormControl('', [Validators.required]),
+    factNameFormControl: new FormControl(),
+    valueLimitFormControl: new FormControl(500, [Validators.required, Validators.max(10000), Validators.min(0)]),
     valuesFormControl: new FormControl('', [Validators.required]),
   });
   currentProject: Project;
   destroyed$: Subject<boolean> = new Subject();
   // tslint:disable-next-line:no-any
   labelSetOptions: any;
+  projectIndices: ProjectIndex[] = [];
+  projectFacts: Subject<string[]> = new Subject();
   @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
 
   constructor(private dialogRef: MatDialogRef<CreateLabelsetDialogComponent>,
               private logService: LogService,
+              private projectService: ProjectService,
               private lexiconService: LexiconService,
               private annotatorService: AnnotatorService,
               private projectStore: ProjectStore) {
@@ -48,30 +59,78 @@ export class CreateLabelsetDialogComponent implements OnInit {
       if (currentProject) {
         this.currentProject = currentProject;
         return forkJoin({
-          options: this.annotatorService.getLabelSetOptions(currentProject.id)
+          options: this.annotatorService.getLabelSetOptions(currentProject.id),
+          projectIndices: this.projectStore.getProjectIndices().pipe(take(1)),
         });
       }
       return of(null);
     })).subscribe(resp => {
-      if (resp) {
-        if (!(resp.options instanceof HttpErrorResponse)) {
-          this.labelSetOptions = resp.options;
-        }
+      if (resp?.options && !(resp?.options instanceof HttpErrorResponse)) {
+        this.labelSetOptions = resp.options;
+      }
+
+      if (resp?.projectIndices && !(resp.projectIndices instanceof HttpErrorResponse)) {
+        this.projectIndices = resp.projectIndices;
+      }
+
+      UtilityFunctions.logForkJoinErrors(resp, HttpErrorResponse, this.logService.snackBarError.bind(this.logService));
+    });
+
+    this.projectStore.getSelectedProjectIndices().pipe(takeUntil(this.destroyed$), switchMap(currentProjIndices => {
+      if (this.currentProject?.id && currentProjIndices) {
+        const indicesForm = this.labelSetForm.get('indicesFormControl');
+        indicesForm?.setValue(currentProjIndices);
+        this.projectFacts.next(['Loading...']);
+        return this.projectService.getProjectFacts(this.currentProject.id, currentProjIndices.map(x => [{name: x.index}]).flat(), false);
+      } else {
+        return of(null);
+      }
+    })).subscribe(resp => {
+      if (resp && !(resp instanceof HttpErrorResponse)) {
+        this.projectFacts.next(resp);
+      } else if (resp) {
+        this.logService.snackBarError(resp, 4000);
       }
     });
+  }
+
+  public indicesOpenedChange(opened: unknown): void {
+    const indicesForm = this.labelSetForm.get('indicesFormControl');
+    // true is opened, false is closed, when selecting something and then deselecting it the formcontrol returns empty array
+    if (!opened && indicesForm?.value) {
+      this.getFactsForIndices(indicesForm?.value);
+    }
+  }
+
+  getFactsForIndices(val: ProjectIndex[]): void {
+    if (val.length > 0) {
+      this.projectFacts.next(['Loading...']);
+      this.projectService.getProjectFacts(this.currentProject.id, val.map((x: ProjectIndex) => [{name: x.index}]).flat(), false).subscribe(resp => {
+        if (resp && !(resp instanceof HttpErrorResponse)) {
+          this.projectFacts.next(resp);
+        } else {
+          this.logService.snackBarError(resp);
+        }
+      });
+    } else {
+      this.projectFacts.next([]);
+    }
   }
 
   onSubmit(formData: OnSubmitParams): void {
     const body = {
       category: formData.categoryFormControl,
+      value_limit: formData.valueLimitFormControl,
+      indices: formData.indicesFormControl.map((x: ProjectIndex) => [x.index]).flat(),
+      ...formData.factNameFormControl ? {fact_names: formData.factNameFormControl} : {},
       values: formData.valuesFormControl.length > 0 ? formData.valuesFormControl.split('\n').filter((x: unknown) => x) : [],
     };
     this.annotatorService.createLabelSet(this.currentProject.id, body).subscribe(x => {
       if (x && !(x instanceof HttpErrorResponse)) {
         this.dialogRef.close(x);
       } else {
-        if (x.error.hasOwnProperty('lexicon')) {
-          this.logService.snackBarMessage(x.error.lexicon.join(','), 5000);
+        if (x.error.hasOwnProperty('values')) {
+          this.logService.snackBarMessage(x.error.values.join(','), 5000);
         } else {
           this.logService.snackBarError(x);
         }
