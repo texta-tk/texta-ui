3b3f918e0e403519c9fc86438b2fe603af5cb8a4
gpaimla <gertpaimla@gmail.com>
2022-05-17 13:28:15 +0300

    test

--- cypress/integration/oauth/uaa.e2e-spec.js
+++ cypress/integration/oauth/uaa.e2e-spec.js
@@ -4,19 +4,15 @@ describe('/oauth and uaa auth tests', function () {
     if (!Cypress.env('test_uaa')) {
       cy.log(`SKIPPING UAA TEST AS test_uaa IS ${Cypress.env('test_uaa')}`)
       this.skip();
-    } else {
-      // Clear sessionStorage, see https://github.com/cypress-io/cypress/issues/413
-      cy.window().then((win) => {
-        win.sessionStorage.clear()
-      })
-      // Clear the cookies to prevent sessions taking over
-      cy.clearCookies({domain: null});
-      // Make the access_token invalid
-      cy.clearLocalStorage('access_token');
-      cy.clearLocalStorage('refresh_token');
+    }
+  });
 
+
+  it('should be able to log in and out with uaa', function () {
+    // Verify logged in
+
+    cy.session('test', () => {
       cy.visit('/');
-
       // Navigate to the UAA login
       cy.get('[data-cy=appSharedLoginDialogUaaLogin]').should('exist').click();
       cy.url().should("include", "uaa/login");
@@ -31,65 +27,98 @@ describe('/oauth and uaa auth tests', function () {
         cy.wait('@loginDo').then(x => {
           expect(x.response.statusCode).to.eq(302);
         });
+        cy.wait(5000);
+        cy.get('[data-cy=appNavbarLoggedInUserMenu]').should('be.visible').click();
+        // Logout
+        cy.intercept('POST', 'logout').as('logout');
+        cy.get('[data-cy=appNavbarlogOutMenuItem]').should('be.visible').click();
+        cy.wait('@logout');
+
+        // Verify logged out
+        cy.get('[data-cy=appNavbarLoggedInUserMenu]').should('not.exist');
       });
-      cy.wait(5000);
-    }
-  });
-
-
-  it('should be able to log in and out with uaa', function () {
-    // Verify logged in
-    cy.get('[data-cy=appNavbarLoggedInUserMenu]').should('be.visible').click();
-    // Logout
-    cy.intercept('POST', 'logout').as('logout');
-    cy.get('[data-cy=appNavbarlogOutMenuItem]').should('be.visible').click();
-    cy.wait('@logout');
-
-    // Verify logged out
-    cy.get('[data-cy=appNavbarLoggedInUserMenu]').should('not.exist');
+    })
   });
 
 
   it('should refresh the uaa access_token with the refresh token', function () {
-    // Verify logged in
-    cy.get('[data-cy=appNavbarLoggedInUserMenu]').should('be.visible');
+    cy.session('test2', () => {
+      cy.visit('/');
+      // Navigate to the UAA login
+      cy.get('[data-cy=appSharedLoginDialogUaaLogin]').should('exist').click();
+      cy.url().should("include", "uaa/login");
+      cy.intercept('POST', '/uaa/login.do').as('loginDo');
+      // Get the uaa users fixture
+      cy.fixture("uaa-user").then(uaaUser => {
+        // Get the "email" (actually username) form field
+        cy.get('[name="username"]').should('exist').type(uaaUser.username);
+        cy.get('[name="password"]').should('exist').type(uaaUser.password);
+        // Click the login button
+        cy.get('.island-button').should('exist').click();
+        cy.wait('@loginDo').then(x => {
+          expect(x.response.statusCode).to.eq(302);
+        });
+        cy.wait(5000);
+        cy.get('[data-cy=appNavbarLoggedInUserMenu]').should('be.visible');
 
-    cy.intercept('POST', '**/uaa/refresh-token/').as('refreshToken');
-    cy.intercept('GET', '**/rest-auth/user/').as('userInfo');
+        cy.intercept('POST', '**/uaa/refresh-token/').as('refreshToken');
+        cy.intercept('GET', '**/rest-auth/user/').as('userInfo');
 
-    // Clear the cookies to prevent sessions taking over
-    cy.clearCookies({domain: null});
-    // Make the access_token invalid
-    cy.clearLocalStorage('access_token');
+        // Clear the cookies to prevent sessions taking over
+        cy.clearCookies({domain: null});
+        // Make the access_token invalid
+        cy.clearLocalStorage('access_token');
 
-    // Reload the window
-    cy.reload();
-    // Wait for the refreshToken request
-    cy.wait('@refreshToken');
-    // Wait for the user info request
-    cy.wait('@userInfo');
-    // Check if the user is still logged in after refreshing the token
-    cy.get('[data-cy=appNavbarLoggedInUserMenu]').should('be.visible');
+        // Reload the window
+        cy.reload();
+        // Wait for the refreshToken request
+        cy.wait('@refreshToken');
+        // Wait for the user info request
+        cy.wait('@userInfo');
+        // Check if the user is still logged in after refreshing the token
+        cy.get('[data-cy=appNavbarLoggedInUserMenu]').should('be.visible');
+      });
+    })
   });
 
 
   it('should log out when the uaa access_token and refresh_token are both invalid', function () {
     // Verify logged in
-    cy.get('[data-cy=appNavbarLoggedInUserMenu]').should('be.visible');
+    cy.session('test3', () => {
+      cy.visit('/');
+      // Navigate to the UAA login
+      cy.get('[data-cy=appSharedLoginDialogUaaLogin]').should('exist').click();
+      cy.url().should("include", "uaa/login");
+      cy.intercept('POST', '/uaa/login.do').as('loginDo');
+      // Get the uaa users fixture
+      cy.fixture("uaa-user").then(uaaUser => {
+        // Get the "email" (actually username) form field
+        cy.get('[name="username"]').should('exist').type(uaaUser.username);
+        cy.get('[name="password"]').should('exist').type(uaaUser.password);
+        // Click the login button
+        cy.get('.island-button').should('exist').click();
+        cy.wait('@loginDo').then(x => {
+          expect(x.response.statusCode).to.eq(302);
+        });
+        cy.wait(5000);
+        cy.get('[data-cy=appNavbarLoggedInUserMenu]').should('be.visible');
 
-    cy.intercept('GET', '**/rest-auth/user/').as('userInfo');
+        cy.intercept('GET', '**/rest-auth/user/').as('userInfo');
 
-    // Clear the cookies to prevent sessions taking over
-    cy.clearCookies({domain: null});
-    // Make the access_token invalid
-    cy.clearLocalStorage('access_token');
-    cy.clearLocalStorage('refresh_token');
+        // Clear the cookies to prevent sessions taking over
+        cy.clearCookies({domain: null});
+        // Make the access_token invalid
+        cy.clearLocalStorage('access_token');
+        cy.clearLocalStorage('refresh_token');
 
-    // Reload the window
-    cy.reload();
-    // Wait for the user info request
-    cy.wait('@userInfo');
-    // Check if the user is still logged in after refreshing the token
-    cy.get('[data-cy=appNavbarLoggedInUserMenu]').should('not.exist');
+        // Reload the window
+        cy.reload();
+        // Wait for the user info request
+        cy.wait('@userInfo');
+        // Check if the user is still logged in after refreshing the token
+        cy.get('[data-cy=appNavbarLoggedInUserMenu]').should('not.exist');
+      });
+    })
+
   });
 });
