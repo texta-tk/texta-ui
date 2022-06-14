49199985bfe7a5ad528a3f0c5c4e49d77d022040
gpaimla <gertpaimla@gmail.com>
2022-06-14 21:46:42 +0300

    fix crf-edit karma test

--- src/app/models/crf-extractor/edit-crf-dialog/edit-crf-dialog.component.spec.ts
+++ src/app/models/crf-extractor/edit-crf-dialog/edit-crf-dialog.component.spec.ts
@@ -1,13 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditCrfDialogComponent } from './edit-crf-dialog.component';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {SharedModule} from '../../../shared/shared-module/shared.module';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('EditDialogComponent', () => {
   let component: EditCrfDialogComponent;
   let fixture: ComponentFixture<EditCrfDialogComponent>;
-
+  const mockDialogRef = {
+    close: jasmine.createSpy('close')
+  };
+  const data = {};
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        SharedModule, HttpClientTestingModule, RouterTestingModule, BrowserAnimationsModule
+      ],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: mockDialogRef
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: data
+        }],
       declarations: [ EditCrfDialogComponent ]
     })
     .compileComponents();
