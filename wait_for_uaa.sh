661fdf79618a1182179cb813922724447b1a52fa
wael <wael@texta.ee>
2022-06-21 18:53:12 +0300

    refactor: wait_for_uaa texta-rest backend

--- wait_for_uaa.sh
+++ wait_for_uaa.sh
@@ -9,12 +9,13 @@ until $(curl --output /dev/null --silent --head --fail "$TEXTA_UAA_URL"); do
 done
 echo "UAA server running."
 
-export TEXTA_HEALTH_URL="${TEXTA_HEALTH_URL:-http://localhost/api/v2/health/}"
+export TEXTA_HEALTH_URL="${TEXTA_HEALTH_URL:-http://texta-rest/api/v2/health/}"
 
 # wait for server to start
-until $(curl --output /dev/null --silent --head --fail "$TEXTA_HEALTH_URL"); do
-    echo "Waiting for texta-rest backend."
-    sleep 5
+while ! curl --output /dev/null --silent http://localhost/api/v2/health/
+do
+  echo "Waiting for texta-rest backend."
+  sleep 5
 done
 echo "texta-rest backend running."
 
